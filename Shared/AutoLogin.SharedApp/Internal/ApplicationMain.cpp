5edc382a5ac5ae379bd64f363d5700d8a12d0c20
Дронов Вячеслав <dronovviacheslav@outlook.com>
2015-10-08 11:58:35 +0300

    Working on creating application

--- Shared/AutoLogin.SharedApp/Internal/ApplicationMain.cpp
+++ Shared/AutoLogin.SharedApp/Internal/ApplicationMain.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include <roapi.h>
+#include <Windows.ApplicationModel.h>
+#include <MTL\Client\ComPtr.h>
+#include <MTL\Wrappers\HStringReference.h>
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int) noexcept
 {
-	OutputDebugStringA("Started :-)");
+	using namespace ABI::Windows::Foundation;
+	using namespace ABI::Windows::ApplicationModel::Core;
+	using namespace ABI::Windows::ApplicationModel;
+	using namespace MTL::Client;
+	using namespace MTL::Wrappers;
+
+	RoInitialize(RO_INIT_MULTITHREADED);
+
+	ComPtr<ICoreApplication> coreApplication;
+	GetActivationFactory(HStringReference(RuntimeClass_Windows_ApplicationModel_Core_CoreApplication).Get(),
+						 &coreApplication);
 }
